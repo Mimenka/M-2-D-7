ccdcd3c41f9e3af2360727ed1166b04e089dd04c
Айтурган Кадыралиева <ajturgankadyralieva@Ajturgans-MacBook-Pro.local>
2024-04-01 19:46:23 +0600

    task2 done

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-my_list = ['MOSCOW','BISHKEK','ALMATY','BERLIN','ANKARA']
-def make_to_lower(my_list):
-    return my_list.lower()
-new_list = list(map(lambda lst: lst.lower(),my_list))
+# 1.
+# my_list = ['MOSCOW','BISHKEK','ALMATY','BERLIN','ANKARA']
+# def make_to_lower(my_list):
+#     return my_list.lower()
+# new_list = list(map(lambda lst: lst.lower(),my_list))
+# print(new_list)
+
+# 2.
+my_list = [4,3,6,8,9,7,1,2,34,5,56,76]
+new_list = list(filter(lambda lst: (lst%2==0),my_list))
+new_list2 = list(map(lambda pow: pow**3,new_list))
 print(new_list)
+print(new_list2)
